e04f315a4d6c0d2f664e6f2f6ecc45227664453d
thelanranger <37283974+thelanranger@users.noreply.github.com>
2019-08-01 16:10:59 -0400

    Add files via upload

---
+++ Win10Custom.5b.bat
@@ -0,0 +1,3 @@
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v Path /t REG_SZ /d "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v ExecutionPolicy /t REG_SZ /d "RemoteSigned" /f
+powershell %~dp0win10custom.5b.ps1
