ecb27aecf831851ab63afd76b2c5800239e4bb50
thelanranger <37283974+thelanranger@users.noreply.github.com>
2019-08-01 16:10:18 -0400

    Delete Win10Custom.4b.bat

--- Win10Custom.4b.bat
+++
@@ -1,4 +0,0 @@
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v Path /t REG_SZ /d "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v ExecutionPolicy /t REG_SZ /d "RemoteSigned" /f
-powershell %~dp0win10custom.4b.Computer.ps1
-powershell %~dp0win10custom.4b.User.ps1
